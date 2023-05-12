b3617d8c6a47726e775228419fd927d9440659b1
faleialina <august3@mail.ru>
2023-05-12 22:06:51 +0300

    get

--- src/controller/environment.controller.js
+++ src/controller/environment.controller.js
@@ -2,9 +2,9 @@ const express = require('express');
 const { getAllEnvironment } = require('../service/environment.service.js');
 const route = express.Router();
 
-route.get('/', async (req, res) => {
+route.get('/', async(req, res)=>{
     const data = await getAllEnvironment();
     res.send(data);
-});
+} )
 
 module.exports = route;
--- src/service/environment.service.js
+++ src/service/environment.service.js
@@ -3,6 +3,6 @@ const { getAllEnvironmentDb } = require('../repository/environment.repository.js
 async function getAllEnvironment() {
     const data = await getAllEnvironmentDb();
     return data;
-};
+}
 
-module.exports = { getAllEnvironment }
+module.exports = { getAllEnvironment };
