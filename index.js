051da8984859d90ad0ae7030a7a44298e836b4fb
faleialina <august3@mail.ru>
2023-05-16 00:10:15 +0300

    express-environment-pg/.env

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
 const app = require('./src/app.js');
 
-app.listen(3000, () => console.log('server is run'));
+const port = process.env.PORT;
+app.listen(port, () => console.log('server is run'));
