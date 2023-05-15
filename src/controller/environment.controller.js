e912da8417c714e09ee395ff8ed977f4d4b60d5c
faleialina <august3@mail.ru>
2023-05-16 00:19:23 +0300

    express-environment-pg/try_catch

--- src/controller/environment.controller.js
+++ src/controller/environment.controller.js
@@ -3,34 +3,55 @@ const { getAllEnvironment, getEnvironmentById, createEnvironment, updateEnvironm
 const route = express.Router();
 
 route.get('/', async (req, res) => {
-    const data = await getAllEnvironment();
-    res.send(data);
+    try {
+        const data = await getAllEnvironment();
+        res.status(200).send(data);
+    } catch (error) {
+        res.status(404).send(error.message);
+    }
 });
 
 route.get('/:id', async (req, res) => {
-    const { id } = req.params;
-    const data = await getEnvironmentById(id);
-    res.send(data);
+    try {
+        const { id } = req.params;
+        const data = await getEnvironmentById(id);
+        res.status(200).send(data);
+    } catch (error) {
+        res.status(404).send(error.message);
+    }
 });
 
 route.post('/', async (req, res) => {
-    const { label, category, priority } = req.body;
-    const data = await createEnvironment(label, category, priority);
-    res.send(data);
+    try {
+        const { label, category, priority } = req.body;
+        const data = await createEnvironment(label, category, priority);
+        res.status(200).send(data);
+    } catch (error) {
+        res.status(404).send(error.message);
+    }
 });
 
 route.put('/:id', async (req, res) => {
-    const { id } = req.params;
-    const { label, category, priority } = req.body;
-    const data = await updateEnvironment(id, label, category, priority);
-    res.send(data);
+    try {
+        const { id } = req.params;
+        const { label, category, priority } = req.body;
+        const data = await updateEnvironment(id, label, category, priority);
+        res.status(200).send(data);
+    } catch (error) {
+        res.status(404).send(error.message);
+    }
 });
 
 route.delete('/:id', async (req, res) => {
-    const { id } = req.params;
-    const data = await deleteEnvironment(id);
-    res.send(data);
+    try {
+        const { id } = req.params;
+        const data = await deleteEnvironment(id);
+        res.status(200).send(data);
+    } catch (error) {
+        res.status(404).send(error.message);
+    }
 });
+
 
 module.exports = route;
 
