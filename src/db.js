21374f1716f25aeda08931cc4cefcbabff57b9eb
faleialina <august3@mail.ru>
2023-05-16 23:33:10 +0300

    express-environment-pg/dotenv

--- src/db.js
+++ src/db.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg')
+require('dotenv').config();
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'skills',
-    password: 'Falinka1603',
-    port: '5432'
+    user: process.env.user_DB,
+    host: process.env.host_DB,
+    database: process.env.database_DB,
+    password: process.env.password_DB,
+    port: process.env.port_DB
 })
 
 module.exports = { pool }
