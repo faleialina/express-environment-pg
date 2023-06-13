0ca70ad26bf9c7b3f1ad9bc07b1b5583ddf9f3fb
faleialina <august3@mail.ru>
2023-06-13 22:18:58 +0300

    express-environment-pg/English

--- src/helper/validation.js
+++ src/helper/validation.js
@@ -1,19 +1,19 @@
 function isValidEnvironmentId(req, res, next) {
   const { id } = req.params;
-  if (isNaN(id)) throw new Error('id не число');
-  if (id <= 0) throw new Error('id отрицательный');
+  if (isNaN(id)) throw new Error('id is not a number');
+  if (id <= 0) throw new Error('id is a negative number');
   next();
 }
 
 function isValidEnvironmentBody(req, res, next) {
   const { label, category, priority } = req.body;
-  if (!label) throw new Error('label  отсутствует');
-  if (!category) throw new Error('category  отсутствует');
-  if (!priority) throw new Error('priority  отсутствует');
+  if (!label) throw new Error('label is missing');
+  if (!category) throw new Error('category is missing');
+  if (!priority) throw new Error('priority is missing');
 
-  if (!isNaN(label)) throw new Error('label число');
-  if (!isNaN(category)) throw new Error('category число');
-  if (isNaN(priority)) throw new Error('priority не число');
+  if (!isNaN(label)) throw new Error('label is a number');
+  if (!isNaN(category)) throw new Error('category is a number');
+  if (isNaN(priority)) throw new Error('priority is not a number');
   next();
 }
 module.exports = { isValidEnvironmentId, isValidEnvironmentBody };
--- src/service/environment.service.js
+++ src/service/environment.service.js
@@ -8,31 +8,31 @@ const {
 
 async function getAllEnvironment() {
   const data = await getAllEnvironmentDb();
-  if (!data.length) throw new Error('массив data пустой');
+  if (!data.length) throw new Error('data array is empty');
   return data;
 }
 
 async function getEnvironmentById(id) {
   const data = await getEnvironmentByIdDb(id);
-  if (!data.length) throw new Error('такого id нет');
+  if (!data.length) throw new Error('no such id');
   return data;
 }
 
 async function createEnvironment(label, category, priority) {
   const data = await createEnvironmentDb(label, category, priority);
-  if (!data.length) throw new Error('данные не сохранены');
+  if (!data.length) throw new Error('data not saved');
   return data;
 }
 
 async function updateEnvironment(id, label, category, priority) {
   const data = await updateEnvironmentDb(id, label, category, priority);
-  if (!data.length) throw new Error('такого id нет');
+  if (!data.length) throw new Error('no such id');
   return data;
 }
 
 async function deleteEnvironment(id) {
   const data = await deleteEnvironmentDb(id);
-  if (!data.length) throw new Error('такого id нет');
+  if (!data.length) throw new Error('no such id');
   return data;
 }
 
